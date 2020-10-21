66b8a88c99ac477337e95c22e73639e32f7a5621
thijsfranck <thijsfranck@gmail.com>
2020-10-21 06:58:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-5qq3lj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-5qq3lj)
---
+++ game/make-educated-guess.ts
@@ -0,0 +1,61 @@
+import DefaultMap from "mnemonist/default-map";
+import { enumerate } from "../utils";
+
+export interface Turn {
+  readonly guess: string;
+  readonly cows: number;
+  readonly bulls: number;
+}
+
+export function makeEducatedGuess(
+  turns: Iterable<Turn>,
+  solutionSpace: Iterable<string>
+) {
+  const [bullsWeight, cowsWeight] = weighSymbols(turns);
+  return makeGuess(bullsWeight, cowsWeight, solutionSpace);
+}
+
+function weighSymbols(turns: Iterable<Turn>) {
+  const bullsWeight = new DefaultMap<string, DefaultMap<number, number>>(
+    () => new DefaultMap(() => 0)
+  );
+  const cowsWeight = new DefaultMap<string, number>(() => 0);
+
+  for (const { bulls, cows, guess } of turns) {
+    for (const [key, index] of enumerate(guess)) {
+      const bullsWeightMap = bullsWeight.get(key);
+      bullsWeightMap.set(index, bullsWeightMap.get(index) + bulls);
+      cowsWeight.set(key, cowsWeight.get(key) + cows);
+    }
+  }
+  return [bullsWeight, cowsWeight] as const;
+}
+
+function makeGuess(
+  bullsWeight: DefaultMap<string, DefaultMap<number, number>>,
+  cowsWeight: DefaultMap<string, number>,
+  solutionSpace: Iterable<string>
+) {
+  let alternatives = [],
+    maxScore = -Infinity;
+
+  for (const solution of solutionSpace) {
+    let solutionScore = 0;
+
+    for (const [key, index] of enumerate(solution)) {
+      solutionScore -= cowsWeight.get(key) + bullsWeight.get(key).get(index);
+      if (solutionScore < maxScore) break;
+    }
+
+    if (solutionScore === maxScore) alternatives.push(solution);
+
+    if (solutionScore > maxScore) {
+      alternatives = [solution];
+      maxScore = solutionScore;
+    }
+  }
+
+  const guess = Math.floor(Math.random() * alternatives.length);
+
+  return alternatives[guess];
+}
